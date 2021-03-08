05b835aaf38eb4039bfd092adf2eb30f5af8d315
Cristian Azócar <caam.1429@gmail.com>
2021-03-08 17:21:54 -0300

    Improve code

--- backend/src/storage/RedisStorage/RedisStorage.ts
+++ backend/src/storage/RedisStorage/RedisStorage.ts
@@ -6,13 +6,15 @@ export class RedisStorage implements IStorage {
   private client: WrappedNodeRedisClient;
   private host: string;
   private port: number;
+  private url: string;
   private keyExpiration: number;
 
   constructor() {
-    const { host, port, keyExpiration } = config.redis;
+    const { host, port, url, keyExpiration } = config.redis;
 
     this.host = host;
     this.port = port;
+    this.url = url;
     this.keyExpiration = keyExpiration;
   }
 
@@ -20,8 +22,12 @@ export class RedisStorage implements IStorage {
     if (!this.client) {
       this.client = createNodeRedisClient(config.redis);
       this.client.nodeRedis.on('connect', (): void => {
+        const hostname: string = this.url
+          ? this.url
+          : `${this.host}:${this.port}`;
+
         // eslint-disable-next-line no-console
-        console.log(`Connected to Redis server on ${this.host}:${this.port}`);
+        console.log(`Connected to Redis server on ${hostname}`);
       });
     }
   }
