3039c68df041480aee35b229acad468f034b1099
Cristian Azócar <caam.1429@gmail.com>
2021-08-03 12:52:42 -0400

    Fix backend build bug

--- backend/src/services/HealthService/HealthService.ts
+++ backend/src/services/HealthService/HealthService.ts
@@ -1,19 +1,13 @@
 import { IHealthInfo } from 'src/types/interfaces';
-import pkg from '../../../package.json';
 
 export default class HealthService {
   async getHealthInfo(): Promise<IHealthInfo> {
     return {
       nodeVersion: process.version,
-      service: pkg.name,
       memory: process.memoryUsage(),
       pid: process.pid,
       uptime: process.uptime(),
       environment: process.env.NODE_ENV,
-      package: {
-        name: pkg.name,
-        version: pkg.version,
-      },
     };
   }
 }
