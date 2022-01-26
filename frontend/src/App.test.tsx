1abf8d2e3556cc0a835cab1df5a658429709f2ad
Cristian Azócar <caam.1429@gmail.com>
2022-01-26 16:31:04 -0300

    Update broken tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 test('renders App component', (): void => {
-  render(<App />);
+  render(<App />, {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
   const appElement: HTMLElement = screen.getByTestId('app');
   expect(appElement).toBeInTheDocument();
 });
